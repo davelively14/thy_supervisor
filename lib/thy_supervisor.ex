84970bbd070ce84ba65d56cffcf40cd2f98471f4
Dave Lively <davelively14@gmail.com>
2016-09-29 12:03:17 -0400

    Built basic functionality, still need to add start_children

--- lib/thy_supervisor.ex
+++ lib/thy_supervisor.ex
@@ -1,2 +1,47 @@
 defmodule ThySupervisor do
+  use GenServer
+
+  # API
+
+  def start_link(child_spec_list) do
+    GenServer.start_link(__MODULE__, [child_spec_list])
+  end
+
+  def start_child(supervisor, child_spec) do
+    GenServer.call(supervisor, {:start_child, child_spec})
+  end
+
+  # Callback Functions
+
+  def init([child_spec_list]) do
+    # Trap exits in order to receive exit signals from children as normal
+    # messages.
+    Process.flag(:trap_exit, true)
+    state = child_spec_list
+            |> start_children
+            |> Enum.into(HashDict.new)
+    {:ok, state}
+  end
+
+  def handle_call({:start_child, child_spec}, _from, state) do
+    case start_child(child_spec) do
+      {:ok, pid} ->
+        new_state = state |> HashDict.put(pid, child_spec)
+        {:reply, {:ok, pid}, new_state}
+      :error ->
+        {:reply, {:error, "error starting child"}, state}
+    end
+  end
+
+  # Private functions
+
+  defp start_child({mod, fun, args}) do
+    case apply(mod, fun, args) do
+      pid when is_pid(pid) ->
+        Process.link(pid)
+        {:ok, pid}
+      _ ->
+        :error
+    end
+  end
 end
